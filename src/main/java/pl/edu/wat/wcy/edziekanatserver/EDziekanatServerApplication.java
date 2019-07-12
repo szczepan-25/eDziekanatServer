22fb10732a2820642bc51e3d6361d6aedd7f148b
pawel szczepanik <pawel.szczepanik@student.wat.edu.pl>
2019-07-12 21:56:53 +0200

    init commit

---
+++ src/main/java/pl/edu/wat/wcy/edziekanatserver/EDziekanatServerApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.wat.wcy.edziekanatserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EDziekanatServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EDziekanatServerApplication.class, args);
+    }
+
+}
